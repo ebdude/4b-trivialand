5cd065d01e9a18b65b7af4c9ad6dbb1a08f1fcfc
Emma Blando <emma.blando@warriorlife.net>
2018-01-23 14:05:24 -0800

    adding roll die button

--- webapp.js
+++ webapp.js
@@ -1,1 +1,26 @@
 // JavaScript Document
+
+var dice= {
+	sides: 6,
+	roll: function () {
+		var randomNumber =
+	Math.floor(Math.random() *
+	this.sides) + 1;
+		return randomNumber;
+	}
+}
+
+function printNumber(number) {
+	var placeholder =
+document.getElementById('placeholder');
+	placeholder.innerHTML=
+	number;
+}
+
+var button=
+document.getElementById('button');
+
+button.onclick= function() {
+	var result= dice.roll();
+	printNumber(result);
+};
