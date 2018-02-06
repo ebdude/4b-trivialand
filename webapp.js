45072d5cfccaa4d0423b9e70f01d99d7f92b74b8
Nayan Nair <nayan.nair@warriorlife.net>
2018-02-06 14:20:51 -0800

    Added a couple of questions and answers

--- webapp.js
+++ webapp.js
@@ -76,80 +76,43 @@ function randomQ() {
 
 		case 3:
 			// question 4
-			document.getElementById("demo").innerHTML = "(130-5)/25";
+			document.getElementById("demo").innerHTML = "What is the largest three integer prime number?";
 			// answers
-			document.getElementById("a1").innerHTML = "7";
-			document.getElementById("a1").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a2").innerHTML = "8";
-			document.getElementById("a2").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a3").innerHTML = "6";
-			document.getElementById("a3").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a4").innerHTML = "5";
-			document.getElementById("a4").innerHTML = ("click", rightAnswer);
+			document.getElementById("a1").innerHTML = "882";
+			document.getElementById("a1").addEventListener("click", wrongAnswer);
+			document.getElementById("a2").innerHTML = "997";
+			document.getElementById("a2").addEventListener("click", rightAnswer);
+			document.getElementById("a3").innerHTML = "743";
+			document.getElementById("a3").addEventListener("click", wrongAnswer);
+			document.getElementById("a4").innerHTML = "721";
+			document.getElementById("a4").addEventListener("click", wrongAnswer);
 			break;
-			
 		case 4:
 			// question 5
-			document.getElementById("demo").innerHTML = "What is the largets 3 digit prime number?";
-			// answers
-			document.getElementById("a1").innerHTML = "142";
-			document.getElementById("a1").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a2").innerHTML = "997";
-			document.getElementById("a2").innerHTML = ("click", rightAnswer);
-			document.getElementById("a3").innerHTML = "569";
-			document.getElementById("a3").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a4").innerHTML = "625";
-			document.getElementById("a4").innerHTML = ("click", wrongAnswer);
-			break;
-			
-		case 5:
-			// question 6
-			document.getElementById("demo").innerHTML = "If you flip a coin trhee times what is the probability of getting at least one tail?";
+			document.getElementById("demo").innerHTML = "If you flip a coin three times, what is the probability of getting at least one tail?";
 			// answers
 			document.getElementById("a1").innerHTML = "7/8";
-			document.getElementById("a1").innerHTML = ("click", rightAnswer);
+			document.getElementById("a1").addEventListener("click", rightAnswer);
 			document.getElementById("a2").innerHTML = "4/5";
-			document.getElementById("a2").innerHTML = ("click", wrongAnswer);
+			document.getElementById("a2").addEventListener("click", wrongAnswer);
 			document.getElementById("a3").innerHTML = "3/2";
-			document.getElementById("a3").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a4").innerHTML = "5";
-			document.getElementById("a4").innerHTML = ("click", wrongAnswer);
+			document.getElementById("a3").addEventListener("click", wrongAnswer);
+			document.getElementById("a4").innerHTML = "1/2";
+			document.getElementById("a4").addEventListener("click", wrongAnswer);
 			break;
-			
-		case 6:
-			// question 7
-			document.getElementById("demo").innerHTML = "What is the term for a solid's figure with flat faces that are polygons?";
-			// answers
-			document.getElementById("a1").innerHTML = "Rhombus";
-			document.getElementById("a1").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a2").innerHTML = "Polyhedrons";
-			document.getElementById("a2").innerHTML = ("click", rightAnswer);
-			document.getElementById("a3").innerHTML = "Ugh-hedrons";
-			document.getElementById("a3").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a4").innerHTML = "Tranquilizers";
-			document.getElementById("a4").innerHTML = ("click", wrongAnswer);
-			break;
-			
-		case 7:
-			// question 7
-			document.getElementById("demo").innerHTML = "In our solar system, which two planets are known as ice giants?";
-			// answers
-			document.getElementById("a1").innerHTML = "I do not care";
-			document.getElementById("a1").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a2").innerHTML = "Mars and Jupiter";
-			document.getElementById("a2").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a3").innerHTML = "Saturn and Jupiter";
-			document.getElementById("a3").innerHTML = ("click", wrongAnswer);
-			document.getElementById("a4").innerHTML = "Uranus and Neptune";
-			document.getElementById("a4").innerHTML = ("click", rightAnswer);
-			break;
-			
 		
-			
-			
-			
-			
-			
+		case 5:
+			// question 6
+			document.getElementById("demo").innerHTML = "What is the term for a solid figure with flat faces that are polygons?";
+			// answers
+			document.getElementById("a1").innerHTML = "Paragons";
+			document.getElementById("a1").addEventListener("click", wrongAnswer);
+			document.getElementById("a2").innerHTML = "Aragon";
+			document.getElementById("a2").addEventListener("click", wrongAnswer);
+			document.getElementById("a3").innerHTML = "Polyhedron";
+			document.getElementById("a3").addEventListener("click", rightAnswer);
+			document.getElementById("a4").innerHTML = "Ugh-hedron";
+			document.getElementById("a4").addEventListener("click", wrongAnswer);
 	}
 }
 
