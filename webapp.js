a85f33d0797de5859af0016fc30ac742c99ecb61
Nayan Nair <nayan.nair@warriorlife.net>
2018-02-16 14:00:34 -0800

    Started work for cookies

--- webapp.js
+++ webapp.js
@@ -249,6 +249,11 @@ function wrongAnswer() {
 	document.getElementById("progressBar").value = score;
 }
 
-//PERCENTAGE BAR
-	var percentage = (score/1000) * 100;
-	
+//COOKIES
+
+function setCookie (cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
